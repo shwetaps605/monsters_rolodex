85659d6631ee53899bd2c9e2b8f33c9eb6ea89ac
shwetaps605 <shwetaroy305@gmail.com>
2021-01-12 16:44:24 +0530

    Adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import {Component} from 'react';
+
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+
+class App extends Component{
+
+  constructor() {
+    //calling the super fuction allows to access this.state variables
+    super();
+    
+    this.state = {
+      monsters : [],
+      searchField : " "
+    };
+
+    //this.handleChange = this.handleChange.bind(this);
+    //we cannot bind every function that we write in order to set the context
+    // that's when we use arrow functions
+  }
+
+
+  // arrow functions set the context too where he function was defined in the first place
+  handleChange = (e) => {
+    this.setState({searchField:e.target.value})
+  }
+
+  //componentDidMount is a react life cycle method
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}))
+  }
+
+  render() {
+
+    //Destructuring: same as const monsters = this.state.monsters;
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    
+
+    return (
+      
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder= "Search monsters" handleChange = {this.handleChange} />
+        <CardList monsters= {filteredMonsters}></CardList>
+      </div>
+    )
+
+  }
 }
 
 export default App;
